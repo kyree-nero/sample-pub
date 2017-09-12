e66260f9eccc3571f32889573cc183b2ca1c7775
x <x@x.com>
2017-09-11 20:01:54 -0400

    x

--- web-sample-app/src/main/java/sample/web/controllers/SampleRestController.java
+++ web-sample-app/src/main/java/sample/web/controllers/SampleRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import sample.services.SampleService;
+import sample.web.domain.Sample;
 import sample.web.domain.SampleJsonResponse;
 
 @RestController
@@ -14,14 +15,18 @@ public class SampleRestController {
 
 	@Autowired SampleService sampleService;
 	
-	 @RequestMapping(value="/sample", method = RequestMethod.GET)
-	 @ResponseBody
-	public SampleJsonResponse<String> sampleGet() {
-		 SampleJsonResponse<String> response = new SampleJsonResponse<String>();
-		 response.setData("hi from sample");
+	 @RequestMapping(value="/sample", method = RequestMethod.GET) @ResponseBody
+	public SampleJsonResponse<Sample> sampleGet() {
+		 System.out.println("in service");
+		 SampleJsonResponse<Sample> response = new SampleJsonResponse<Sample>();
+		 Sample sample = new Sample();
+		 sample.setContent("hi from sample.");
+		 response.setData(sample);
 		 
 		 sampleService.doStuff();
 		 sampleService.findCountInDb();
 		 return response;
 	 }
+	 
+	 
 }
---
+++ web-sample-app/src/main/java/sample/web/domain/Sample.java
@@ -0,0 +1,21 @@
+package sample.web.domain;
+
+public class Sample {
+	private String content;
+	
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
