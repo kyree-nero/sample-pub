aed952c2c41c1524d859a0e7f816405155c9a6aa
x <x@x.com>
2017-10-11 22:17:11 -0400

    pretty print on tests, README changes, Soapui project

--- websvc-sample-application/src/test/java/sample/SampleWebServiceIT.java
+++ websvc-sample-application/src/test/java/sample/SampleWebServiceIT.java
@@ -1,19 +1,10 @@
 package sample;
 
 import java.io.ByteArrayOutputStream;
-import java.io.StringReader;
 
-import javax.xml.namespace.QName;
 import javax.xml.soap.MessageFactory;
-import javax.xml.soap.SOAPElement;
-import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.Source;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.soap.SOAPHeaderElement;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,13 +25,7 @@ public class SampleWebServiceIT extends AbstractWebServiceIT {
     @Autowired private Marshaller marshaller;
     
     
-    public  Document loadXMLFrom(String xml) throws TransformerException {
-    	
-        Source source = new StreamSource(new StringReader(xml));
-        DOMResult result = new DOMResult();
-        TransformerFactory.newInstance().newTransformer().transform(source , result);
-        return (Document) result.getNode();
-    }   
+   
     
 	@Test public void findSamples() throws Exception {
 		//make request
@@ -49,33 +34,17 @@ public class SampleWebServiceIT extends AbstractWebServiceIT {
 		StringResult requestString = new StringResult();
 		marshaller.marshal(findSampleRequest, requestString);
 
-		System.out.println(requestString);
-		
 		Document content = loadXMLFrom(requestString.toString());
-		
 		SOAPMessage soapMessage = MessageFactory.newInstance().createMessage();
 		soapMessage.getSOAPBody().addDocument(content);
-
-		SOAPEnvelope envelope = soapMessage.getSOAPPart().getEnvelope();
-		String securityNamespace = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
-		envelope.addNamespaceDeclaration("wsse", securityNamespace);
-	
-		QName securityQName = soapMessage.getSOAPHeader().createQName("Security", "wsse");
-		SOAPHeaderElement securityElement = soapMessage.getSOAPHeader().addHeaderElement(securityQName);
-		QName userNameTokenQName = soapMessage.getSOAPHeader().createQName("UsernameToken", "wsse");
-		SOAPElement userNameTokenElement = securityElement.addChildElement(userNameTokenQName);
-		QName userNameQName = soapMessage.getSOAPHeader().createQName("Username", "wsse");
-		SOAPElement userNameElement = userNameTokenElement.addChildElement(userNameQName);
-		userNameElement.addTextNode("joe");
-		QName passwordTokenQName = soapMessage.getSOAPHeader().createQName("Password", "wsse");
-		SOAPElement passwordTokenElement = userNameTokenElement.addChildElement(passwordTokenQName);
-		passwordTokenElement.addTextNode("blow");
+		addUsernameTokenSecurityHeader(soapMessage);
+		
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		soapMessage.writeTo(outputStream);
-		String output = new String(outputStream.toByteArray());
+		String input = new String(outputStream.toByteArray());
 		
-		System.out.println(output);
-		Source requestPayload = new StringSource(output);
+		System.out.println(prettyPrint(input));
+		Source requestPayload = new StringSource(input);
 		
 		
 		//make response
@@ -89,7 +58,7 @@ public class SampleWebServiceIT extends AbstractWebServiceIT {
 		StringResult responseString = new StringResult();
 		marshaller.marshal(findSampleResponse, responseString);
 
-		System.out.println(responseString);
+		System.out.println(prettyPrint(responseString.toString()));
 		Source responsePayload = new StringSource(responseString.toString());
 		
 		//do test
