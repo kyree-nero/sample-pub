a4a857237aa54e3436bb54322db634bbf4d649a7
x <x@x.com>
2017-10-20 17:24:32 -0400

    corrections so jobs can be repeatable

--- batch-sample-application/src/main/java/sample/web/controllers/BatchController.java
+++ batch-sample-application/src/main/java/sample/web/controllers/BatchController.java
@@ -4,8 +4,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionException;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
@@ -25,27 +27,37 @@ public class BatchController {
 	@Autowired  @Qualifier("simpleRemotePartitionBatchJob") Job simpleRemotePartitionBatchJob;
 	@Autowired JobLauncher jobLauncher;
 	@Autowired JobExplorer jobExplorer;
+//	@Autowired JobOperator jobOperator;
 	
 	
 	@GetMapping(path = "batch/simplebatchjob/start", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public  Long startSimpleBatchJob() throws JobExecutionException {
-		JobExecution jobExecution = jobLauncher.run(simpleBatchJob, new JobParameters());
+		JobParameters jobParameters = new JobParametersBuilder().addLong("unq", System.nanoTime()).toJobParameters();
+		System.out.println(jobParameters);
+		JobExecution jobExecution = jobLauncher.run(simpleBatchJob, jobParameters);
 		return jobExecution.getId();
+//		return jobOperator.startNextInstance(simpleBatchJob.getName());
 	}
 	
 	@GetMapping(path = "batch/simplepartitioningbatchjob/start", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public  Long startSimplePartitionBatchJob() throws JobExecutionException {
-		JobExecution jobExecution = jobLauncher.run(simplePartitionBatchJob, new JobParameters());
+		JobParameters jobParameters = new JobParametersBuilder().addLong("unq", System.nanoTime()).toJobParameters();
+		System.out.println(jobParameters);
+		JobExecution jobExecution = jobLauncher.run(simplePartitionBatchJob, jobParameters);
 		return jobExecution.getId();
+//		return jobOperator.startNextInstance(simplePartitionBatchJob.getName());
 	}
 	
 	@GetMapping(path = "batch/simpleremotepartitioningbatchjob/start", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public  Long startSimpleRemotePartitionBatchJob() throws JobExecutionException {
-		JobExecution jobExecution = jobLauncher.run(simpleRemotePartitionBatchJob, new JobParameters());
+		JobParameters jobParameters = new JobParametersBuilder().addLong("unq", System.nanoTime()).toJobParameters();
+		System.out.println(jobParameters);
+		JobExecution jobExecution = jobLauncher.run(simpleRemotePartitionBatchJob, jobParameters);
 		return jobExecution.getId();
+//		return jobOperator.startNextInstance(simpleRemotePartitionBatchJob.getName());
 	}
 	
 	@GetMapping(path = "batch/status/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
