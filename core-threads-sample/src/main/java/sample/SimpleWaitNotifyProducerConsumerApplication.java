a543ff62cfd513b89304b1f303f71ae12b6a9f67
x <x@x.com>
2017-12-10 09:52:53 -0500

    edit README

--- core-threads-sample/src/main/java/sample/SimpleWaitNotifyProducerConsumerApplication.java
+++ core-threads-sample/src/main/java/sample/SimpleWaitNotifyProducerConsumerApplication.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
-public class BufferedProducerConsumerApplication {
+public class SimpleWaitNotifyProducerConsumerApplication {
 	public static void main(String[] args) {
 		WidgetBuffer buffer = new WidgetBuffer(10);
 		
